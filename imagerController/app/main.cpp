8d0417ffdf3ae52a9254df9004c8e55c21ad241f
Pmitrega <pawel.mitrega1@wp.pl>
2024-11-14 21:29:14 +0000

    added test with streaming camera

--- imagerController/app/main.cpp
+++ imagerController/app/main.cpp
@@ -2,6 +2,9 @@
 #include <thread>
 #include <fstream>
 #include <chrono>
+#include <iomanip>
+#include <sstream>
+
 
 #include "ASICamera2.h"
 #include <iostream>
@@ -13,8 +16,9 @@
 
 
 void takeTestImages(){
-		CameraController cam_controller;
+	CameraController cam_controller;
 	StarLocator star_locator;
+	MqttClientWrapper mqtt_client;
 	cam_controller.scanForCameras();
 	auto camera_list = cam_controller.getCameraList();
 	std::cout<< camera_list.size() << std::endl;
@@ -54,6 +58,61 @@ void takeTestImages(){
 	}
 }
 
+void streamCameraMQTT(){
+
+	CameraController cam_controller;
+	MqttClientWrapper mqtt_client;
+	cam_controller.scanForCameras();
+	auto camera_list = cam_controller.getCameraList();
+	std::cout<< camera_list.size() << std::endl;
+	for(auto it = camera_list.cbegin(); it < camera_list.cend(); it++ ){
+		std::cout<< (*it) << std::endl;
+	}
+	cam_controller.openCameraByProducerAndID(ASI, 0U);
+	cam_controller.setCameraExposure_us(500'000);
+	cam_controller.setCameraGain(70);
+	cam_controller.setImageType(IMG_RAW16);
+	uint8_t buffer[1280*960*2];
+	int  i = 0;
+	int ms_betw_frames = 3000;
+	while(true){
+        auto capture_start_time =  std::chrono::system_clock::now();
+		auto t = std::time(nullptr);
+		if(CAM_CTRL_FAIL == cam_controller.takeAnImage()){
+			continue;
+		}
+		std::string filename;
+		if(i < 10){
+			filename = std::string("image0") + std::to_string(i) + ".raw";
+		}
+		else{
+			filename = std::string("image") + std::to_string(i) + ".raw";
+		}
+		auto tm = *std::localtime(&t);
+		std::ostringstream oss;
+		oss << std::put_time(&tm, "%d-%m-%Y %H-%M-%S");
+		mqtt_client.publishMessageString("images/raw/title", filename);
+		mqtt_client.publishMessageString("images/raw/capture_time", oss.str());
+		mqtt_client.publishMessageNumber("images/raw/exposure", 500);
+		mqtt_client.publishMessageNumber("images/raw/gain", 70);
+		mqtt_client.publishMessageString("images/raw/cameraType", "ASI");
+		mqtt_client.publishMessageString("images/raw/ROIwidth", "1280");
+		mqtt_client.publishMessageString("images/raw/ROIheight", "960");
+		mqtt_client.publishMessageString("images/raw/ROIstart_x", "0");
+		mqtt_client.publishMessageString("images/raw/ROIstart_y", "0");
+		mqtt_client.publishMessageImageRaw("images/raw", (char*)cam_controller.getBuffer().get(), 1280*960*2);
+		auto capture_end_time =  std::chrono::system_clock::now();
+        std::chrono::duration<float> elapesed_time = capture_end_time - capture_start_time;
+        int sleep_time = (ms_betw_frames) - static_cast<int>(1000*elapesed_time.count());
+		LOG_INFO("sleep time: %d ms", sleep_time)
+        std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
+
+		i++;
+	}
+
+}
+		
+
 void testStepperControllerComm(){
 	stepperCommunicator step_com;
 	step_com.connectSerial("/dev/ttyUSB0");
@@ -89,7 +148,8 @@ void testStepperControllerComm(){
 
 
 int main(){
+	streamCameraMQTT();
 	//testStepperControllerComm();
-	testMQTT();
+	//testMQTT();
     return EXIT_SUCCESS;
 }
