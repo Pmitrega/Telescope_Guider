15a82302166c85004a4ea7746d87e9db92362344
Pmitrega <pawel.mitrega1@wp.pl>
2025-04-25 01:39:49 +0100

    fiexd moniotring

--- imagerController/app/inc/motorMonitor.hpp
+++ imagerController/app/inc/motorMonitor.hpp
@@ -5,14 +5,14 @@ class motorMonitor{
 public:
     void resetMotorsPosition();
     double getPositionRa();
-    double getPositonDec();
+    double getPositionDec();
     void updateRaSpeed(double speed);
     void updateDecSpeed(double speed);
     void updateRaPosition(double t_diff);
     void updateDecPosition(double t_diff);
 private:
     // 144 steps per minute is 15'/s. It means: 15*60 = 900 = 144 * steps_to_deg/3600
-    const double steps_to_deg = 900/144/3600;
+    const double steps_to_deg = 900.0/144.0/3600.0;
     double ra_motor_steps = 0;
     double dec_motor_steps = 0;
 
--- imagerController/app/src/motorMonitor.cpp
+++ imagerController/app/src/motorMonitor.cpp
@@ -1,5 +1,5 @@
 #include "motorMonitor.hpp"
-
+#include "logger.hpp"
 void motorMonitor::resetMotorsPosition(){
     dec_motor_steps = 0;
     ra_motor_speed = 0;
@@ -9,7 +9,8 @@ double motorMonitor::getPositionRa(){
     return ra_motor_steps * steps_to_deg;
 }
 
-double motorMonitor::getPositonDec(){
+double motorMonitor::getPositionDec(){
+    //LOG_INFO("stepstodeg: %f\r\n", steps_to_deg);
     return dec_motor_steps * steps_to_deg;
 }
 
@@ -20,11 +21,11 @@ void motorMonitor::updateRaSpeed(double speed){
 void motorMonitor::updateDecSpeed(double speed){
     dec_motor_speed = speed;
 }
-
 void motorMonitor::updateRaPosition(double t_diff){
-    ra_motor_steps = t_diff/60 * ra_motor_speed;
+    
+    ra_motor_steps += t_diff/60 * ra_motor_speed;
 }
 
 void motorMonitor::updateDecPosition(double t_diff){
-    dec_motor_steps = t_diff/60 * dec_motor_speed;
+    dec_motor_steps += t_diff/60 * dec_motor_speed;
 }
