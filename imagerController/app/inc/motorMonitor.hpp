06c9cddbe152504e0aed07957774a632760359b6
Pawel_Mitrega <pawel.mitrega1@wp.pl>
2025-04-27 21:04:31 +0200

    change for fp

--- imagerController/app/inc/motorMonitor.hpp
+++ imagerController/app/inc/motorMonitor.hpp
@@ -11,8 +11,13 @@ public:
     void updateRaPosition(double t_diff);
     void updateDecPosition(double t_diff);
 private:
+<<<<<<< Updated upstream
     // 144 steps per minute is 15'/s. It means: 15*60 = 900 = 144 * steps_to_deg/3600
     const double steps_to_deg = 900.0/144.0/3600.0;
+=======
+    // 144 steps per minute is 15'/s. It means: 15*60 = 900 = 144 * steps_to_deg*3600
+    const double steps_to_deg = 900/144/3600;
+>>>>>>> Stashed changes
     double ra_motor_steps = 0;
     double dec_motor_steps = 0;
 
